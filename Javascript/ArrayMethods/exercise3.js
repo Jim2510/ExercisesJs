660ff14cfd783ce54e0ed93e9cffef5d9924f646
Jim2510 <gmguglielmino@gmail.com>
2023-12-12 16:40:49 +0100

    File
    Added third exercise

---
+++ Javascript/ArrayMethods/exercise3.js
@@ -0,0 +1,17 @@
+
+/*
+Utilizza il metodo forEach che prende un array di nomi 
+di frutta e li stampa aggiungendo un punto esclamativo 
+a ciascun nome di frutta.
+*/
+
+/*
+! In questo esercizio viene semplicemente usaro un ciclo forEach che 
+! utilizza all'interno una callback che concatena l'elemento ciclato
+! con un punto esclamativo, 
+*/
+
+const fruits = ["Mela", "Banana", "Kiwi", "Fragola"];
+
+const fruits1 = fruits.forEach(fruit => console.log(fruit + '!')) // o console.log(`${fruit}!`)
+
