b4096c09af4cc6b0ffaa229c725a1bae69b1b6a3
Jim2510 <gmguglielmino@gmail.com>
2023-12-13 10:17:52 +0100

    File
    Added second template string exercise

---
+++ Javascript/TemplateStrings/exercise2.js
@@ -0,0 +1,12 @@
+// Utilizza una template string per creare una 
+// stringa su più righe che includa interruzioni 
+// di riga.
+
+
+const str = `
+Questa è una
+stringa con 
+interruzioni
+`
+
+console.log(str)
