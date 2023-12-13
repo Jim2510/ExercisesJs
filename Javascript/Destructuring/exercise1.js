a36a82969ea5ef20be659a5a659996488db5c04e
Jim2510 <gmguglielmino@gmail.com>
2023-12-13 12:37:04 +0100

    File
    Added first destructuring exercise

---
+++ Javascript/Destructuring/exercise1.js
@@ -0,0 +1,11 @@
+// Dato un oggetto person con le proprietà 
+// nome ed età, utilizza la destrutturazione 
+// dell'oggetto per creare variabili nome ed 
+// età che contengano i valori dell'oggetto.
+
+const person = { nome: "Alice", age: 30 };
+
+const {nome, age} = person;
+
+console.log(nome);
+console.log(age);
