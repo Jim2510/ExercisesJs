9961af4677e797c368dd5fce9a30a4d6589bb265
Jim2510 <gmguglielmino@gmail.com>
2023-12-16 17:49:04 +0100

    File
    Added comment to setTimeout exercise4

--- Javascript/SetTimeOutInterval/exercise4.js
+++ Javascript/SetTimeOutInterval/exercise4.js
@@ -6,10 +6,13 @@
 // Prima dello scadere dei 2 secondi stampa in console il messaggio ""Lancio dei dadi in corso..."
 
 
+// dichiarazione del setTimeout che stampa il messaggio di lancio
+
 setTimeout(() => {
     console.log('Lancio dadi in corso...')
 }, 500)
 
+// funzione che genera 2 numeri random da 1 a 6 simulando i dadi e stampa un messaggio con il risultato dei dadi
 
    function lanciaDadi() {
     const dice1 = Math.floor(Math.random() * 6 + 1)
@@ -18,5 +21,6 @@ setTimeout(() => {
 Dado 2 = ${dice2}`)
 }
 
+// chiamata del setTimeout che ritarda l'esecuzione della funzione lanciaDadi di 2 secondi
 
 setTimeout(lanciaDadi, 2000)
