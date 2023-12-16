22990ef5c364f3aa6aebf9d409b1a0aabb6e98e8
Jim2510 <gmguglielmino@gmail.com>
2023-12-16 16:23:19 +0100

    File
    Added fifth callback exercise

---
+++ Javascript/Callback/exercise5.js
@@ -0,0 +1,22 @@
+// Crea una funzione che accetta due numeri come argomenti e una funzione di callback.
+// La funzione dovrebbe eseguire un'operazione matematica sui numeri e passare il risultato alla funzione di callback.
+// Ad esempio, puoi creare una funzione per aggiungere due numeri e passare una funzione di callback che visualizza il risultato.
+
+
+
+function performOperation(a, b, callback) {
+
+    // salvataggio della somma dei parametri in una variabile
+  let result = a + b;
+  // chiamata della callback che passa come parametro il risultato
+  callback(result)
+}
+
+function displayResult(result) {
+    // funzione che stampa il risultato passata come callback
+  console.log(`La somma è: ${result}`)
+}
+
+// chiamata della funzione principale
+
+performOperation(5, 3, displayResult);
