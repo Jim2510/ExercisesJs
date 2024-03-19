53856ac1d3d3cfb198442effca26b98f5af7dc28
Jim2510 <gmguglielmino@gmail.com>
2024-03-19 21:13:07 +0100

    React
    Fetch exercises added

--- React/viteProject/my-app/src/data_fetching/GitHubUsers.jsx
+++ React/viteProject/my-app/src/data_fetching/GitHubUsers.jsx
@@ -1,13 +1,62 @@
+import { useEffect, useState } from "react";
 import { GitHubUser } from "./GitHubUser";
 
 export function GitHubUsers() {
+  const [user, setUser] = useState("");
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchUsername(username) {
+    try {
+      setLoading(true);
+      const res = await fetch(`https://api.github.com/users/${username}`);
+      if (res.ok) {
+        const data = await res.json();
+        setList((prevList) => [...prevList, data]);
+      } else {
+        throw new Error(res.status);
+      }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  //
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await fetchUsername(user);
+    setUser("");
+  }
+
+  function handleChange(event) {
+    setUser(event.target.value);
+  }
+
+  useEffect(() => {
+    if (user) {
+      fetchUsername(user);
+    }
+  }, [user]);
+
   return (
     <>
-      <form action="">
-        <input type="text" />
-        <button type="submit">Search</button>
-      </form>
-      <ul>{GitHubUser}</ul>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={user}
+            onChange={handleChange}
+            placeholder="Enter GitHub username"
+          />
+          <button type="submit">Search</button>
+        </form>
+        {loading && <p>Loading...</p>}
+        {list.map((userData) => (
+          <GitHubUser key={userData.id} username={userData.login} />
+        ))}
+      </div>
     </>
   );
 }
