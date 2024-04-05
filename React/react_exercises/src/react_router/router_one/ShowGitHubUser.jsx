cd09cd71c16e0171560753fa1926898e8c7088ff
Jim2510 <gmguglielmino@gmail.com>
2024-04-05 10:04:11 +0200

    ShowGitHubUser moved to the exercise folder.

--- React/react_exercises/src/react_router/router_one/ShowGitHubUser.jsx
+++ React/react_exercises/src/react_router/router_one/ShowGitHubUser.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GithubUser } from "./GitHubUser";
+
+// Avevo lasciato il componente nella cartella src
 
 export function ShowGitHubUser() {
   const { username } = useParams();
